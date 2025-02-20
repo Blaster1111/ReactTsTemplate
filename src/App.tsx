c0ccbdeb9f0fe99ebc70c89d8b625d64cf6c8562
Blaster1111 <rudrashukla0711@gmail.com>
2025-02-20 12:01:00 +0530

    Basic Templatae

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <Button className="mt-4">CLick Me</Button>
+    </>
+  );
+}
+
+export default App;
