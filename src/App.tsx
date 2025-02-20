6476360145db7818299418694400db9b7e4248a2
Blaster1111 <rudrashukla0711@gmail.com>
2025-02-20 20:39:11 +0530

    more components added

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello world!</h1>
-      <Button className="mt-4">CLick Me</Button>
+      <Button className="forced-color-adjust-auto">Click Me</Button>
     </>
   );
 }
